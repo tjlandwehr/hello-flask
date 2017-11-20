ee40d9c6657c30ab21777ee26da4c9dd8b8ed74d
Timothy Landwehr <tjlandwehr@gmail.com>
2017-11-20 17:02:09 -0600

    Introduction to use of jinja2 environment to create templates for html elements

--- main.py
+++ main.py
@@ -1,30 +1,24 @@
 from flask import Flask, request, redirect
 import cgi
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """
-<!DOCTYPE html>
-<html>
-    <body>
-        <form action="/hello" method="post">
-            <label for="first_name">First Name:</label>
-            <input type="text" name="first_name" />
-            <input type="submit" />
-        </form>
-    </body>
-</html>
-"""
-
 @app.route("/")
 def index():
-    return form
+    template = jinja_env.get_template('hello_form.html')
+    return template.render()
 
 @app.route("/hello", methods=['POST'])
 def hello():
     first_name = request.form['first_name']
-    return '<h1>Hello, ' + cgi.escape(first_name) + '</h1>'
+    template = jinja_env.get_template('hello_greeting.html')
+    return template.render(name=first_name)
 
 time_form = """
     <style>
