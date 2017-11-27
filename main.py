fbb955f2632e8d702df438652efb0e05425d2c4d
Timothy Landwehr <tjlandwehr@gmail.com>
2017-11-26 22:04:38 -0600

    Made the template code more DRY by refactoring into a base template all the "boilerplate" code that is frequently reused, and then using template extensions to expand and individualize that HTML.

--- main.py
+++ main.py
@@ -85,6 +85,6 @@ def todos():
         tasks.append(task)
 
     template = jinja_env.get_template('todos.html')
-    return template.render(tasks=tasks)
+    return template.render(title="TODOs", tasks=tasks)
 
 app.run()
